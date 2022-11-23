2e80d440b86bb1c578883e3bba2ea888c536c12e
Obycoder8 <90784287+Obycoder8@users.noreply.github.com>
2022-11-24 02:46:03 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const app = express();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "containers-us-west-71.railway.app",
+  host: "containers-us-west-61.railway.app",
   user: "root",
-  password: "Ly5AlA40N7LKanhMhclm",
+  password: "Sw80pd0iWseG7ttWzcjk",
   database: "railway",
-  port: "5462",
+  port: "7719"
 });
 
 app.use(cors());
